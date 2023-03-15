3a6e71d7cf44e4bf5930290b6e98c2e746f50a43
20211TIIMI0230 <marianalopesgouvea@gmail.com>
2023-03-15 12:52:50 -0300

    Adicionei comentarios ao codigo

--- app/src/main/java/gouvea/lopes/primeiraapp/MainActivity.java
+++ app/src/main/java/gouvea/lopes/primeiraapp/MainActivity.java
@@ -16,15 +16,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //Declarando
+        //Variavel btnEnviar armazena o botão da activity_main com o id = btnEnviar
         Button bntEnviar = findViewById(R.id.btnEnviar);
+        //metodo setOnClickListener ouvidor de cliques
         bntEnviar.setOnClickListener(new View.OnClickListener() {
             @Override
+            //metodo OnClick vai realizar os proximos comandos assim que o usuário clicar no botão
             public void onClick(View view) {
+                //variavel etDigiteAqui armazena o editor de texto com id = etDigiteAqui
                 EditText etDigiteAqui = findViewById(R.id.etDigiteAqui);
+                //varivel texto armazena o que foi digitado no EditText etDigiteAqui
                 String texto = etDigiteAqui.getText().toString();
+                //criacao da intenção de navegar desta MainActivity para a NextActivity
                 Intent intent = new Intent(MainActivity.this,NextActivity.class);
+                //dicionario com a chave "texo" e com o valor sendo a variavel texto(possui o que foi digitado pelo usuario)
                 intent.putExtra("texto",texto);
+                //realiza a intencao(leva para a NextAtivity)
                 startActivity(intent);
             }
         });
--- app/src/main/java/gouvea/lopes/primeiraapp/NextActivity.java
+++ app/src/main/java/gouvea/lopes/primeiraapp/NextActivity.java
@@ -5,17 +5,20 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.TextView;
-
+//classe que virá o texto que foi digitado na MainActivity
 public class NextActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_next);
-
+        //pega a intent que foi enviada pela MainActivity
         Intent intent = getIntent();
+        //pega a string da intent com a chave "texto" do dicionario
         String texto = intent.getStringExtra("texto");
+        //declarando textView que irá armazenar o texo enviado
         TextView tvTexto = findViewById(R.id.tvTexto);
+        //escrevendo o texto que foi enviado no testview
         tvTexto.setText(texto);
     }
 }
